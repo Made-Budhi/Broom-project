3f44481db77f8401a097c5fe1e27e09a73338a49
Made-Budhi <madebudhi15@gmail.com>
2023-11-16 13:27:20 +0800

    ADPL-3 feat: add new table account

--- db_broom.sql
+++ db_broom.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE db_broom;
 
+-- DROP DATABASE db_broom;
+
 USE db_broom;
 
 /*
@@ -7,77 +9,73 @@ convention yang aku pakek
 https://courses.cs.washington.edu/courses/cse154/18au/resources/styleguide/sql/naming-conventions-sql.html
 */
 
-CREATE TABLE User
-(
-  /*
-   aku malah keinget kalimat pak ambara tadi (30/10/2023), klo data yang di
-   input manual user mendingan jangan dijadiin PK, tapi dibuatin id AI aja
-   tergantung diskusi kita nanti sih
-   */
-  user_id  VARCHAR(18)  NOT NULL UNIQUE,
-  name     VARCHAR(254) NOT NULL,
-  phone    VARCHAR(15)  NOT NULL,
-  role     ENUM (
-    'Mahasiswa', 'Pegawai'
-    )                   NOT NULL,
-  email    VARCHAR(254) NOT NULL,
-  password VARCHAR(32)  NOT NULL,
-
-  PRIMARY KEY (user_id)
+CREATE TABLE Account (
+	account_id 	INT AUTO_INCREMENT,
+    email 		VARCHAR(254) NOT NULL,
+    password 	VARCHAR(254) NOT NULL,
+    role		ENUM('Peminjam', 'Pimpinan', 'Pengelola') NOT NULL,
+    
+    PRIMARY KEY (account_id)
 );
 
-
-
-CREATE TABLE Room
+CREATE TABLE Peminjam
 (
-  room_id INT                  NOT NULL AUTO_INCREMENT,
-  name    VARCHAR(254)         NOT NULL,
-  status  BOOLEAN DEFAULT TRUE NOT NULL,
+	peminjam_id  	CHAR(18)  NOT NULL UNIQUE,
+	name     		VARCHAR(254) NOT NULL,
+	phone    		VARCHAR(15)  NOT NULL,
+	role     		ENUM ('Mahasiswa', 'Pegawai') NOT NULL,
+	account_id		INT UNIQUE NOT NULL,
 
-  PRIMARY KEY (room_id)
+	FOREIGN KEY (account_id) REFERENCES Account(account_id),
+	PRIMARY KEY (peminjam_id)
 );
 
-CREATE TABLE Director
+CREATE TABLE Pimpinan
 (
-  director_id CHAR(18)     NOT NULL UNIQUE,
-  name        VARCHAR(254) NOT NULL,
-  email       VARCHAR(254) NOT NULL,
-  password    VARCHAR(32)  NOT NULL,
-  position    ENUM (
-    'Direktur', 'Direktur 1', 'Direktur 2', 'Direktur 3'
-    /*masih kurang tau*/
-    ),
+	pimpinan_id 	CHAR(18)     NOT NULL UNIQUE,
+	name        	VARCHAR(254) NOT NULL,
+	position		VARCHAR(254) NOT NULL,
+	account_id		INT UNIQUE NOT NULL,
 
-  PRIMARY KEY (director_id)
+	FOREIGN KEY (account_id) REFERENCES Account(account_id),
+	PRIMARY KEY (pimpinan_id)
 );
 
-CREATE TABLE Reservation
-(
-  reserve_id      INT          NOT NULL UNIQUE AUTO_INCREMENT,
-  user_id         VARCHAR(18),
-  room_id         INT,
-  director_id     CHAR(18),
-  document_number VARCHAR(20)  NOT NULL, /*gak tau panjangnya*/
-  date_start      DATETIME     NOT NULL,
-  date_end        DATETIME     NOT NULL,
-  purpose         VARCHAR(254) NOT NULL,
-  status          ENUM (
-    'Diterima', 'Ditolak', 'Menunggu'
-    )                          NOT NULL,
+CREATE TABLE Pengelola (
+	pengelola_id	CHAR(18) NOT NULL,
+    name			VARCHAR(254) NOT NULL,
+    account_id		INT UNIQUE NOT NULL,
 
-  PRIMARY KEY (reserve_id),
-  FOREIGN KEY (user_id) REFERENCES User (user_id),
-  FOREIGN KEY (room_id) REFERENCES Room (room_id),
-  FOREIGN KEY (director_id) REFERENCES Director (director_id)
+	FOREIGN KEY (account_id) REFERENCES Account(account_id),
+    PRIMARY KEY (pengelola_id)
 );
 
-CREATE TABLE Manager
+CREATE TABLE Ruangan
 (
-  manager_id CHAR(18)     NOT NULL UNIQUE,
-  name       VARCHAR(254) NOT NULL,
-  email      VARCHAR(254) NOT NULL,
-  password   VARCHAR(32)  NOT NULL,
+	ruangan_id 	INT                  NOT NULL AUTO_INCREMENT,
+	name   		 VARCHAR(254)         NOT NULL,
+	status  BOOLEAN DEFAULT TRUE NOT NULL,
 
-  PRIMARY KEY (manager_id)
+	PRIMARY KEY (ruangan_id)
+);
+
+CREATE TABLE Reservasi
+(
+	reservasi_id	INT          NOT NULL UNIQUE AUTO_INCREMENT,
+	peminjam_id		VARCHAR(18),
+	ruangan_id      INT,
+	pimpinan_id     CHAR(18),
+	document_number VARCHAR(20)  NOT NULL,
+	date_start      DATETIME     NOT NULL,
+	date_end        DATETIME     NOT NULL,
+	purpose         VARCHAR(254) NOT NULL,
+	status          ENUM (
+		'Diterima', 'Ditolak', 'Menunggu'
+	)                          	NOT NULL,
+
+	PRIMARY KEY (reservasi_id),
+	FOREIGN KEY (peminjam_id) REFERENCES Peminjam (peminjam_id),
+	FOREIGN KEY (ruangan_id) REFERENCES Ruangan (ruangan_id),
+	FOREIGN KEY (pimpinan_id) REFERENCES Pimpinan (pimpinan_id)
 );
 
