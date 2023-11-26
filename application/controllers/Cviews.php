a8eecdaed59237adfb44c33b74dee9789e28238d
okhrisiana <okri>
2023-11-26 23:13:37 +0800

    ADPL-24 Feat: Create register feature and add register button in login

---
+++ application/controllers/Cviews.php
@@ -0,0 +1,19 @@
+<?php
+
+class Cviews extends CI_Controller
+{
+	/**
+	 * Loading login page.
+	 *
+	 * @return void
+	 */
+	function loginpage()
+	{
+		$this->load->view('loginpage');
+	}
+
+	function register()
+	{
+		$this->load->view('Register');
+	}
+}
---
+++ application/views/Loginpage.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Login Page</title>
+</head>
+<body>
+	<h1>LOGIN PAGE</h1>
+
+	<?php
+	// Shows error login message if there's any.
+	$errormsg = $this->session->flashdata('loginerror');
+	if (!is_null($errormsg)) {
+		?>
+
+		<div>
+			<?= $errormsg ?>
+		</div>
+
+		<?php
+	}
+	?>
+
+	<form action="<?= base_url('index.php/clogin/loginauth') ?>" method="post">
+		<label for="email">Enter E-mail</label>
+		<input type="text" name="email" id="email">
+
+		<br>
+
+		<label for="password">Enter Password</label>
+		<input type="password" name="password" id="password">
+
+		<input type="submit" value="Login">
+	</form>
+
+<button type="button" onClick="login()">Daftar</button>
+<script language="javascript">
+	function login()
+	{
+		window.open("<?php echo base_url('Cviews/Register')?>","_self");	
+	}
+</script>
+
+</body>
+</html>
