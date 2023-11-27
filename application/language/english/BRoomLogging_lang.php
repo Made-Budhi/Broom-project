eb02f4b74d414d43644db40221fd0edbe1c71f16
Prajna Paramahansa <satriagama1@gmail.com>
2023-11-27 09:08:00 +0800

    ADPL-25 feat(language): add logging value for info and debugging

---
+++ application/language/english/BRoomLogging_lang.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['log_send_email_failed'] = 'failed to send email';
+$lang['log_send_email_succeed'] = 'succeed to send email';
