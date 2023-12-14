a566b70fc50ec99cf860a34712fe1c46f56e8827
Made Budhi <madebudhi15@gmail.com>
2023-12-14 12:46:24 +0800

    style(model): reformat code

--- application/models/Mreservasi.php
+++ application/models/Mreservasi.php
@@ -1,36 +1,30 @@
 <?php
 
-    class Mreservasi extends CI_Model 
-    {
-        function tampildata()
-		{
-			$sessions = $this->session->get_userdata();
-			
-			$query= $this->db->select()->from("reservasi")->join(
-				"peminjam",
-				"reservasi.peminjam_id = peminjam.id",
-				"inner"
-				)->where("peminjam_id",$sessions['id'])->get();
-			
-            // to check query database
-			if ($query->num_rows()>0)
-			{
-                // put the quest to variable
-				foreach ($query->result() as $row)
-				{
-					$hasil[]=$row;
-				}	
+class Mreservasi extends CI_Model
+{
+	function tampildata()
+	{
+		$sessions = $this->session->get_userdata();
+
+		$query = $this->db->select()->from("reservasi")->join(
+			"peminjam",
+			"reservasi.peminjam_id = peminjam.id",
+			"inner"
+		)->where("peminjam_id", $sessions['id'])->get();
+
+		// to check query database
+		if ($query->num_rows() > 0) {
+			// put the quest to variable
+			foreach ($query->result() as $row) {
+				$hasil[] = $row;
 			}
-			else
-			{
-				$hasil="";	
-			}
-            // return variable to get database
-			return $hasil;	
+		} else {
+			$hasil = "";
 		}
+		// return variable to get database
+		return $hasil;
+	}
 
-
-
-    }
+}
 
 ?>
