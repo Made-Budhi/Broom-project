24c9fe8de5ebb6ba2cdb4324cb9b918215ef32e8
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-14 18:40:35 +0800

    refactor(models): change the code for readability

--- application/models/Mreservasi.php
+++ application/models/Mreservasi.php
@@ -1,36 +1,29 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-    class Mreservasi extends CI_Model 
-    {
-        function tampildata()
-		{
-			$sessions = $this->session->get_userdata();
-
-			$query= $this->db->select()->from("reservasi")->join(
-				"peminjam",
-				"reservasi.peminjam_id = peminjam.id",
+/**
+ * @property CI_Session $session
+ * @property CI_DB $db
+ */
+class Mreservasi extends CI_Model
+{
+	function tampildata(): string|array
+	{
+		$sessions = $this->session->get_userdata();
+		$hasil = array();
+		
+		$query = $this->db->select()->from("Reservasi")->join(
+				"Peminjam",
+				"Reservasi.peminjam_id = Peminjam.id",
 				"inner"
-				)->where("peminjam_id",$sessions['id'])->get();
-
-            // to check query database
-			if ($query->num_rows()>0)
-			{
-                // put the quest to variable
-				foreach ($query->result() as $row)
-				{
-					$hasil[]=$row;
-				}	
-			}
-			else
-			{
-				$hasil="";	
-			}
-            // return variable to get database
-			return $hasil;	
+		)->where("peminjam_id", $sessions['id'])->get();
+		
+		// to check query database
+		foreach ($query->result() as $row) {
+			$hasil[] = $row;
 		}
-
-
-
-    }
-
-?>
+		
+		// return variable to get database
+		return $hasil;
+	}
+}
