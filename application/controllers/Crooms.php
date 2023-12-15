0797bf6e96b118e8b73d799968ba2d7bcdf4ba5f
Made Budhi <madebudhi15@gmail.com>
2023-12-16 00:03:30 +0800

    feat(controller): combine every pages with menu sidebar

--- application/controllers/Crooms.php
+++ application/controllers/Crooms.php
@@ -1,36 +1,43 @@
 <?php
-	class Crooms extends CI_Controller
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @property Mrooms $rooms
+ */
+class Crooms extends CI_Controller
+{
+	public function __construct()
 	{
-		function tampil()
-		{
-			$this->load->model("Mrooms");
-			$tampilgedung['hasil']=$this->Mrooms->tampilgedung();
-			$data['konten'] = $this->load->view("roomlist", $tampilgedung, TRUE);
-			$this->load->view("layouts/sidebar", $data);
-		}
-		
-        function calendar($gedung)
-		{
-			$this->load->model("Mrooms");
-			$tampildata['hasil']=$this->Mrooms->tampildata($gedung);
-			$data = array(
-				'tabel' => $this->load->view('schedule',$tampildata,TRUE),
-				'gedung' => $gedung
-			);
-			// $data['tabel']=$this->load->view('schedule',$tampildata,TRUE);
-			$this->load->view('calendar',$data);
-		}
-
-		function view(){
-			$data = array(
-				'gedung' => $this->input->get('gedung')
-			);
-
-			// echo $this->input->get('gedung');
-
-			$this->load->view('calendar', $data);
-		}
+		parent::__construct();
+		$this->load->model("Mrooms", "rooms");
 	}
 
-        
-?>
+	function index(): void
+	{
+		$tampilgedung['hasil'] = $this->rooms->tampilgedung();
+		$data['konten'] = $this->load->view("menu_peminjam/roomlist", $tampilgedung, TRUE);
+		$this->load->view("layouts/sidebar", $data);
+	}
+
+	function calendar($id): void
+	{
+		$tampildata['hasil'] = $this->rooms->tampildata($id);
+		$data = array(
+			'tabel' => $this->load->view('menu_peminjam/schedule', $tampildata, TRUE),
+			'id' => $id
+		);
+
+		$data['konten'] = $this->load->view('menu_peminjam/calendar', $data, true);
+		$this->load->view('layouts/sidebar', $data);
+	}
+
+	function view(): void
+	{
+		$data = array(
+			'id' => $this->input->get('id')
+		);
+
+		$data['konten'] = $this->load->view('menu_peminjam/calendar', $data, true);
+		$this->load->view('layouts/sidebar', $data);
+	}
+}
