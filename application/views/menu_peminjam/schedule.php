a19c0c3d39079da3817431a2b266940b3ed0e512
Made Budhi <madebudhi15@gmail.com>
2023-12-16 00:07:39 +0800

    feat(views): adjustment to accessing the database to correct object

--- application/views/menu_peminjam/schedule.php
+++ application/views/menu_peminjam/schedule.php
@@ -3,15 +3,14 @@
     <thead>
       <tr>
         <th>No</th>
-        <th>Title</th>
-        <th>Desc</th>
-        <th>Start</th>
-        <th>End</th>
+        <th>Tanggal dan Waktu Mulai</th>
+        <th>Tanggal dan Waktu Selesai</th>
         <th>Aksi</th>
       </tr>
     </thead>
     <tbody>
     <?php
+
 		if(empty($hasil))
 		{
 			echo "Data Kosong";	
@@ -23,12 +22,10 @@
 	?>
       <tr>
         <td><?php echo $no; ?></td>
-        <td><?php echo $data->title ?></td>
-        <td><?php echo $data->description ?></td>
-        <td><?php echo $data->start_datetime ?></td>
-        <td><?php echo $data->end_datetime ?></td>
+        <td><?php echo format_indo($data->date_start) . ', ' . $data->time_start ?></td>
+        <td><?php echo format_indo($data->date_end) . ', ' . $data->time_end ?></td>
         <td>
-        <button type="button" class="btn btn-sm btn-primary">Ajukan</button>
+        <button type="button" class="btn btn-sm btn-primary">Kontak Peminjam</button>
         </td>
       </tr>
       
