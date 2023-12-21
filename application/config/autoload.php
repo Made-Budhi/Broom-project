53425ffe2d7e1ad78d2565e4805b97f8ee1943e8
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-21 16:24:29 +0800

    ADPL-37 chore(config): load enum/constant helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,8 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'form', 'date', 'tanggal', 'view');
+$autoload['helper'] = array('url', 'file', 'form', 'date', 'tanggal', 'view',
+		'BRoomConst');
 
 /*
 | -------------------------------------------------------------------
