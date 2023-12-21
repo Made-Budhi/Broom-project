13114b2d03c56fa811cf6f0273113f64ac09e377
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-21 16:23:35 +0800

    ADPL-37 feat(helpers): add BRoom enum/constant helper

---
+++ application/helpers/BRoomConst_helper.php
@@ -0,0 +1,21 @@
+<?php
+
+class StatusReservasi
+{
+	const DITERIMA = 'Diterima';
+	const DITOLAK = 'Ditolak';
+	const MENUNGGU = 'Menunggu';
+}
+
+class AccountRole
+{
+	const PEMINJAM = 'Peminjam';
+	const PIMPINAN = 'Pimpinan';
+	const PENGELOLA = 'Pengelola';
+}
+
+class PeminjamRole
+{
+	const MAHASISWA = 'Mahasiswa';
+	const PEGAWAI = 'Pegawai';
+}
