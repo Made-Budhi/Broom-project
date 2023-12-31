214c31da0b3881e52e58842e62d86e4cf3e0dcf9
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-31 13:50:57 +0800

    style(controllers): fix code indentation

--- application/controllers/Chalaman.php
+++ application/controllers/Chalaman.php
@@ -14,35 +14,34 @@ class Chalaman extends Broom_Controller
 	 * @return void
 	 */
 	public function otp(): void
-  {
+	{
 		$this->load->view('otp');
 	}
-
+	
 	/**
 	 * Determines the validity of the otp code
 	 *
 	 * @return void
 	 */
 	public function reset(): void
-  {
+	{
 		/*
 		 * Checking whether the otp is the correct one or not.
 		 * If correct		= go to reset password page
 		 * If not			= go back to otp input page
 		 */
-		if($this->session->userdata('token')==$this->input->post('token')) {
+		if ($this->session->userdata('token') == $this->input->post('token')) {
 			$this->load->view('reset');
-		}
-		else{
+		} else {
 			redirect('chalaman/otp');
 		}
 	}
-
+	
 	/**
 	 * Calling model for resetting password
 	 */
 	public function newpass(): void
-  {
+	{
 		$this->account->newpass($this->input->post('password'));
 	}
 }
