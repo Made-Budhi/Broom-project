819c30165b483cf4f43df53a4b4d19eb42e5ad88
[oxeDNich] <[okrisiana13@gmail.com]>
2024-01-07 00:41:43 +0800

    ADPL-49 Style(Views): mengubah dari card ke table

--- application/views/reservasi_peminjam.php
+++ application/views/reservasi_peminjam.php
@@ -1,44 +1,62 @@
 <!-- load bootstrap, font-awesome, style, googlefont  -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="<?= base_url('assets/styles/sidebar.css')?>">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="<?= base_url('assets/styles/sidebar.css')?>">
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Gabarito&family=Geologica&display=swap');
 </style>
 
+<h1>Data Reservasi Peminjam</h1>
 
-<h3 class="w-90 mx-auto">History Reservasi</h3>
+<br>
 
 <?php
-view_data($hasil, 'name', 'purpose', 'date_start', 'reservasi_id');
+view_data($hasil, 'name', 'purpose', 'reservasi_status', 'reservasi_id');
 if($hasil == NULL){
   echo "data kosong";
 }
+?>
 
+<table class="table table-bordered" >
+
+  <thead>
+  <th class="text-center" scope="col">No</th>
+  <th class="text-center" scope="col">Nama Ruangan</th>
+  <th class="text-center" scope="col">Waktu</th>
+  <th class="text-center" scope="col">Status</th>
+  <th class="text-center" scope="col">Aksi</th>
+  </thead>
+
+<?php
 $no=1;
 foreach ($hasil as $data):
 ?>
+  <tbody>
 
-<div class="card mb-3 w-90 mx-auto" style="justify-content-center">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-        <div class="card-body d-flex justify-content-between align-items-center">
-          <div>
-            <h4> Nama Ruangan: <?php echo $data->name ?></h4>
-            <p class="card-text">Perihal : <?php echo $data->purpose ?></p>
-            <p class="card-text">Date Start : <?php echo $data->date_start ?></p>
-          </div>
+      <tr>
+        <td class="text-center">
+          <?php echo $no ?>
+        </td>
+        <td class="text-center">
+          <?php echo $data->name ?>
+        </td>
+        <td class="text-center">
+          <?php echo $data->date_start ?>
+        </td>
+        <td class="text-center">
+          <?php echo $data->reservasi_status ?>
+        </td>
+        <td class="text-center">
           <a target="_blank" href="<?= site_url('reservation/document/' . $data->reservasi_id) ?>" class="btn btn-primary">Detail dokumen</a>
-        </div>
-    </div>
-  </div>
-</div>
+        </td>
+      </tr>
 
 <?php
 $no++;
 endforeach;
 ?>
+
+  </tbody>
+</table>
+
