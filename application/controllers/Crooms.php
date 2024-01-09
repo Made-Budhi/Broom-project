602cf197c93bc95d5546c848063ecca1164fc2b6
Prajna Paramahansa <satriagama1@gmail.com>
2024-01-10 02:18:59 +0800

    ADPL-48 feat: add new rooms and searching from database

--- application/controllers/Crooms.php
+++ application/controllers/Crooms.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * @property Mrooms $rooms
  * @property CI_Input $input
+ * @property CI_Session $session
+ * @property CI_Upload $upload
  */
 class Crooms extends Broom_Controller
 {
@@ -43,4 +45,36 @@ class Crooms extends Broom_Controller
 		$data['content'] = $this->load->view('menu_peminjam/calendar', $data, true);
 		$this->load->view('layouts/sidebar', $data);
 	}
+	
+	function search(): void
+	{
+		$search = $this->input->get('room_name');
+		$this->rooms->search($search);
+	}
+	
+	function add(): void
+	{
+		if (empty($this->input->post())) {
+			redirect('rooms');
+			return;
+		}
+		
+		// Upload room image
+		$config = array(
+				'upload_path' 	=> FCPATH.'assets/images/ruangan/',
+				'allowed_types' => 'jpg|png',
+				'max_size' 		=> 0,
+				'max_width'		=> 0,
+				'max_height'	=> 0
+		);
+		
+		$this->load->library('upload', $config);
+		$data['image'] = upload_handler($this->upload, $config,
+				$this->view, 'image');
+		
+		$this->rooms->add_room($data);
+		
+		redirect('rooms');
+	}
+	
 }
