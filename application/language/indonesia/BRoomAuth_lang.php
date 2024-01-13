3a7502143f4ae5015d498fdae66726b47d8ea8f3
Prajna Paramahansa <satriagama1@gmail.com>
2024-01-13 18:56:11 +0800

    ADPL-54 feat: add nim and nik duplicate checker

--- application/language/indonesia/BRoomAuth_lang.php
+++ application/language/indonesia/BRoomAuth_lang.php
@@ -1,9 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['register_success'] = 'Data berhasil disimpan, Silahkan melakukan '.
+$lang['register_success'] = 'Data berhasil disimpan, Silahkan <br> melakukan'.
 		'Verifikasi melalui email yang telah didaftarkan';
 $lang['email_not_verified'] = 'Email belum terverifikasi!';
 $lang['login_failed'] = 'E-mail atau Password tidak valid';
 $lang['forgot_pass_failed'] = 'E-mail tidak valid atau belum terverifikasi';
+$lang['register_duplicate_id'] = 'NIK atau NIM pengguna telah terdaftar';
 
